411300bf83f3167c7f14954fd59559c8434a2cef
Josh Scoville <ag4sm@outlook.com>
2023-01-22 14:02:23 -0500

    Initial Commit

---
+++ Braces/BracesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BracesApp.swift
+//  Braces
+//
+//  Created by Josh Scoville on 1/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BracesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
