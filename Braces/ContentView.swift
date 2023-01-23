739f5f766302179e1793e549139539af865fe4ea
Josh Scoville <ag4sm@outlook.com>
2023-01-23 14:00:52 -0500

    setup of initial stack with color and title case

--- Braces/ContentView.swift
+++ Braces/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    let mainColor = Color(red: 120/255, green: 128/255, blue: 158/255)
+    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            mainColor.ignoresSafeArea()
+            VStack {
+                Text("Can I eat with my braces?")
+                    .bold()
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+            }
+            Spacer()
+            .padding()
+            
         }
-        .padding()
+        .foregroundColor(.white)
     }
 }
 
