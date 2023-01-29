9b087fb7eb243b9594e31386d8f957b4e0045729
Josh Scoville <ag4sm@outlook.com>
2023-01-28 21:08:17 -0500

    Create separate file to hold Dictionary.

---
+++ Braces/FoodDict.swift
@@ -0,0 +1,18 @@
+//
+//  FoodDict.swift
+//  Braces
+//
+//  Created by Josh Scoville on 1/28/23.
+//
+
+import Foundation
+
+let foodItems: [String:String] = [
+    "Candy": "Hard candy can break off braces from teeth.  No do not eat.",
+    "Gum": "Gum can get caught all up in wires and bands and is a sticky mess.  No, do not eat.",
+    "Meat": "Yes, you can eat tender, well cut up meat and you need the protein for your health.  Avoid stringy and tough cuts of meat and do not eat from the bone.",
+    "Nuts": "Avoid nuts and seeds as these can get hung in your wires and between your teeth and be painful.",
+    "Vegetables": "Avoid stringy vegetables and raw hard ones.",
+    "Bread": "You can eat as long as it's soft.  Avoid hard crusty breads",
+    "Chips": "Certain chips can be eaten, but use caution as some may creat a gummy paste that sticks to brackets and bands."
+]
